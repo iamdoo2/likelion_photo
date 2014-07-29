0385a519842d75cb0c7bdfa2edf12c97df5d8c84
pjhjohn <pjhjohn@gmail.com>
2014-07-29 14:43:22 +0900

    [SYG] Update Teacher description (namdy not respond)

--- snu-yonsei.rb
+++ snu-yonsei.rb
@@ -1,9 +1,9 @@
 @snu-yonsei = Array.new
 teaachers = [
-	["홍지호", "연세대학교 컴퓨터과학과", "snu-yonsei/t1.jpg", "한마디"],
-	["박준호", "서울대학교 컴퓨터공학부", "snu-yonsei/t1.jpg", "한마디"],
-	["한영후", "Georgia Tech Computer Science", "snu-yonsei/t1.jpg", "한마디"],
-	["남다예", "연세대학교 컴퓨터과학과", "snu-yonsei/t1.jpg", "한마디"]
+	["홍지호", "연세대학교 컴퓨터과학과", "snu-yonsei/t1.jpg", "수고가 많다"],
+	["박준호", "서울대학교 컴퓨터공학부", "snu-yonsei/t2.jpg", "밤샘이 많다"],
+	["한영후", "Georgia Tech Computer Science", "snu-yonsei/t3.jpg", "고민중..."],
+	["남다예", "연세대학교 컴퓨터과학과", "snu-yonsei/t4.jpg", "403 Forbidden"]
 ].shuffle
 students = [
 	["강지혜", "연세대학교 경영학과", 			"snu-yonsei/s0.jpg" ],
@@ -11,7 +11,7 @@ students = [
 	["김다예", "서울대학교 건축학과", 			"snu-yonsei/s2.jpg" ],
 	["김대홍", "서울대학교 건축학과", 			"snu-yonsei/s3.jpg" ],
 	["김민석", "서울대학교 산업공학과", 		"snu-yonsei/s4.jpg" ],
-	["노승은", "서울대학교 자유전공학부(컴퓨터공학/경제학)", 	"snu-yonsei/s5.jpg" ],
+	["노승은", "서울대학교 자유전공학부", 		"snu-yonsei/s5.jpg" ],
 	["류기현", "서울대학교 경영학과", 			"snu-yonsei/s6.jpg" ],
 	["류재희", "연세대학교 컴퓨터과학과", 		"snu-yonsei/s7.jpg" ],
 	["백재현", "연세대학교 전기전자공학", 		"snu-yonsei/s8.jpg" ],
@@ -20,11 +20,11 @@ students = [
 	["이교영", "서울대학교 지역시스템공학과", 	"snu-yonsei/s11.jpg"],
 	["이동우", "서울대학교 기계항공공학부", 	"snu-yonsei/s12.jpg"],
 	["이종복", "연세대학교 경제학과", 			"snu-yonsei/s13.jpg"],
-	["이혜리", "숙명여자대학교 멀티미디어과학과","snu-yonsei/s14.jpg"],
+	["이혜리", "숙명여대 멀티미디어과학과",		"snu-yonsei/s14.jpg"],
 	["정승완", "중앙대학교 정치국제학과", 		"snu-yonsei/s15.jpg"],
 	["정우주", "연세대학교 정보산업공학과", 	"snu-yonsei/s16.jpg"],
-	["조경욱", "서울대학교 시각디자인", 		"snu-yonsei/s17.jpg"],
-	["천현정", "이화여자대학교 약학과",			"snu-yonsei/s18.jpg"],
+	["조경욱", "서울대학교 시각디자인과", 		"snu-yonsei/s17.jpg"],
+	["천현정", "이화여대 약학과",				"snu-yonsei/s18.jpg"],
 	["현지인", "서울대학교 서양화과", 			"snu-yonsei/s19.jpg"]
 ].shuffle
 @snu-yonsei << teachers << students
